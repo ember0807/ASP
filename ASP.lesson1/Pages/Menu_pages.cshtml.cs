8efc28e1f0d47c71c63f848016bc9ce17b90715a
XDavy_la41 <XDavy_la41@top-academy.ru>
2025-04-28 13:14:51 +0300

    изменено только меню , страницу с адресами не меняла

--- ASP.lesson1/Pages/Menu_pages.cshtml.cs
+++ ASP.lesson1/Pages/Menu_pages.cshtml.cs
@@ -1,12 +1,46 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace ASP.lesson1.Pages
 {
-    public class Index1Model : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class MenuItem
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public decimal Price { get; set; }
+	}
+
+	public class RestaurantMenuService
+	{
+		public List<MenuItem> GetMenuItems()
+		{
+			return new List<MenuItem>
+			{
+				new MenuItem { Id = 1, Name = "Sushi", Price = 500 },
+				new MenuItem { Id = 2, Name = "Ramen", Price = 450 },
+				new MenuItem { Id = 3, Name = "Tempura", Price = 600 },
+				new MenuItem { Id = 4, Name = "Takoyaki", Price = 400 },
+				new MenuItem { Id = 5, Name = "Miso Soup", Price = 250 },
+			};
+		}
+	}
+
+	public class Index1Model : PageModel
+	{
+		private readonly RestaurantMenuService _restaurantMenuService;
+
+		public Index1Model(RestaurantMenuService restaurantMenuService)
+		{
+			_restaurantMenuService = restaurantMenuService;
+		}
+
+		// Добавлено свойство для хранения элементов меню
+		public List<MenuItem> MenuItems { get; private set; }
+
+		public void OnGet()
+		{
+			MenuItems = _restaurantMenuService.GetMenuItems();
+		}
+	}
 }
--- ASP.lesson1/Program.cs
+++ ASP.lesson1/Program.cs
@@ -1,38 +1,42 @@
+using ASP.lesson1.Pages;
+
 namespace ASP.lesson1
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddRazorPages();
+			// Регистрация служб
+			builder.Services.AddRazorPages();
+			builder.Services.AddScoped<RestaurantMenuService>(); // Регистрация сервиса
 
-            var app = builder.Build();
+			var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+			// Настройка конвейера обработки HTTP запросов
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Error");
+				app.UseHsts();
+			}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseAuthorization();
 
-            app.MapRazorPages();
+			app.MapRazorPages();
+
 			app.MapGet("/", context =>
 			{
 				context.Response.Redirect("/Home_pages"); // путь на случай отсутствия индекс файла
 				return Task.CompletedTask;
 			});
+
 			app.Run();
-        }
-    }
+		}
+	}
 }
